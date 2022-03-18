24b3053ac1fdb33f77d14458553711c55e0412f0
codephillip <codephillip@gmail.com>
2022-03-18 15:40:14 +0300

    added campaigns, email and subscriptions endpoints

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,9 +1,76 @@
 import express, {Request, Response} from 'express';
+import axios from 'axios';
+import {emailBaseUrl, requestError, subscriptionBaseUrl} from "../utils/constants/stringUtils";
 
 const router = express.Router();
 
 router.get('/api/' + process.env.API_VERSION + '/emails', async (req: Request, res: Response) => {
-  res.status(200).send("email service");
+  const config = req.headers
+  // @ts-ignore
+  axios.get(emailBaseUrl + req.path, req.headers)
+    .then(response => {
+      res.status(response.status).send(response.data);
+    })
+    .catch(error => {
+      res.status(400).send({'message': requestError})
+    })
+});
+
+router.post('/api/' + process.env.API_VERSION + '/emails', async (req: Request, res: Response) => {
+  // @ts-ignore
+  axios.post(emailBaseUrl + req.path, req.body, req.headers)
+    .then(response => {
+      res.status(response.status).send(response.data);
+    })
+    .catch(error => {
+      res.status(400).send({'message': requestError})
+    })
+});
+
+router.get('/api/' + process.env.API_VERSION + '/subscriptions', async (req: Request, res: Response) => {
+  const config = req.headers
+  // @ts-ignore
+  axios.get(subscriptionBaseUrl + req.path, req.headers)
+    .then(response => {
+      res.status(response.status).send(response.data);
+    })
+    .catch(error => {
+      res.status(400).send({'message': requestError})
+    })
+});
+
+router.post('/api/' + process.env.API_VERSION + '/subscriptions', async (req: Request, res: Response) => {
+  // @ts-ignore
+  axios.post(subscriptionBaseUrl + req.path, req.body, req.headers)
+    .then(response => {
+      res.status(response.status).send(response.data);
+    })
+    .catch(error => {
+      res.status(400).send({'message': requestError})
+    })
+});
+
+router.get('/api/' + process.env.API_VERSION + '/campaigns', async (req: Request, res: Response) => {
+  const config = req.headers
+  // @ts-ignore
+  axios.get(subscriptionBaseUrl + req.path, req.headers)
+    .then(response => {
+      res.status(response.status).send(response.data);
+    })
+    .catch(error => {
+      res.status(400).send({'message': requestError})
+    })
+});
+
+router.post('/api/' + process.env.API_VERSION + '/campaigns', async (req: Request, res: Response) => {
+  // @ts-ignore
+  axios.post(subscriptionBaseUrl + req.path, req.body, req.headers)
+    .then(response => {
+      res.status(response.status).send(response.data);
+    })
+    .catch(error => {
+      res.status(400).send({'message': requestError})
+    })
 });
 
 export {router as indexResourceRouter};
---
+++ src/server/utils/constants/stringUtils.ts
@@ -0,0 +1,9 @@
+const emailBaseUrl = 'http://adidas-express-email-srv:4002'
+const subscriptionBaseUrl = 'http://adidas-express-sub-srv:4001'
+const requestError = 'Can\'t process request'
+
+export {
+  emailBaseUrl,
+  subscriptionBaseUrl,
+  requestError
+};
