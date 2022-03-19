67d8c62f9fc6617615b72ae60a8b2762620fb3f1
codephillip <codephillip@gmail.com>
2022-03-19 05:38:06 +0300

    added authentication, refactored then and catch on axios functions

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import {json} from 'body-parser';
 import {emailRouter} from "../server/routes/emailRouter";
 import {subscriptionRouter} from "../server/routes/subscriptionRouter";
+import {currentUser} from '@adidastest-phillip/common'
 
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
+app.use(currentUser);
 app.use(emailRouter);
 app.use(subscriptionRouter);
 
--- src/server/routes/emailRouter.ts
+++ src/server/routes/emailRouter.ts
@@ -1,74 +1,50 @@
 import express, {Request, Response} from 'express';
 import axios from 'axios';
-import {emailBaseUrl, requestError} from "../utils/constants/stringUtils";
+import {emailBaseUrl, requestError} from '../utils/constants/stringUtils';
+import {requireAuth} from '@adidastest-phillip/common'
 
 const router = express.Router();
 
-process.env.API_VERSION = 'v1'
-router.get(`/api/${process.env.API_VERSION}/emails`, async (req: Request, res: Response) => {
+router.get(`/api/${process.env.API_VERSION}/emails`, requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.get(emailBaseUrl + req.path, req.headers)
-    .then(response => {
-      res.status(response.status).send(response.data);
-    })
-    .catch(error => {
-      res.status(400).send({'message': requestError})
-    })
+    .then(response => res.status(response.status).send(response.data))
+    .catch(error => res.status(400).send({'message': requestError}))
 });
 
-router.get(`/api/${process.env.API_VERSION}/emails/:id`, async (req: Request, res: Response) => {
+router.get(`/api/${process.env.API_VERSION}/emails/:id`, requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.get(emailBaseUrl + req.path, req.headers)
-    .then(response => {
-      res.status(response.status).send(response.data);
-    })
-    .catch(error => {
-      res.status(400).send({'message': requestError})
-    })
+    .then(response => res.status(response.status).send(response.data))
+    .catch(error => res.status(400).send({'message': requestError}))
 });
 
-router.post(`/api/${process.env.API_VERSION}/emails`, async (req: Request, res: Response) => {
+router.post(`/api/${process.env.API_VERSION}/emails`, requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.post(emailBaseUrl + req.path, req.body, req.headers)
-    .then(response => {
-      res.status(response.status).send(response.data);
-    })
-    .catch(error => {
-      res.status(400).send({'message': requestError})
-    })
+    .then(response => res.status(response.status).send(response.data))
+    .catch(error => res.status(400).send({'message': requestError}))
 });
 
-router.patch(`/api/${process.env.API_VERSION}/emails/:id`, async (req: Request, res: Response) => {
+router.patch(`/api/${process.env.API_VERSION}/emails/:id`, requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.patch(emailBaseUrl + req.path, req.body, req.headers)
-    .then(response => {
-      res.status(response.status).send(response.data);
-    })
-    .catch(error => {
-      res.status(400).send({'message': requestError})
-    })
+    .then(response => res.status(response.status).send(response.data))
+    .catch(error => res.status(400).send({'message': requestError}))
 });
 
-router.put(`/api/${process.env.API_VERSION}/emails/:id`, async (req: Request, res: Response) => {
+router.put(`/api/${process.env.API_VERSION}/emails/:id`, requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.put(emailBaseUrl + req.path, req.body, req.headers)
-    .then(response => {
-      res.status(response.status).send(response.data);
-    })
-    .catch(error => {
-      res.status(400).send({'message': requestError})
-    })
+    .then(response => res.status(response.status).send(response.data))
+    .catch(error => res.status(400).send({'message': requestError}))
 });
 
-router.delete(`/api/${process.env.API_VERSION}/emails/:id`, async (req: Request, res: Response) => {
+router.delete(`/api/${process.env.API_VERSION}/emails/:id`, requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.delete(emailBaseUrl + req.path, req.body, req.headers)
-    .then(response => {
-      res.status(response.status).send(response.data);
-    })
-    .catch(error => {
-      res.status(400).send({'message': requestError})
-    })
+    .then(response => res.status(response.status).send(response.data))
+    .catch(error => res.status(400).send({'message': requestError}))
 });
 
 export {router as emailRouter};
