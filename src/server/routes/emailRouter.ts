f3f604eeb11e7370f78aaa8bb9e8aab4bc7c79fa
codephillip <codephillip@gmail.com>
2022-03-19 14:18:45 +0300

    fixed error status codes and response data

--- src/server/routes/emailRouter.ts
+++ src/server/routes/emailRouter.ts
@@ -9,35 +9,35 @@ router.get([`/api/${process.env.API_VERSION}/emails/:id`, `/api/${process.env.AP
   // @ts-ignore
   axios.get(emailBaseUrl + req.path, req.headers)
     .then(response => res.status(response.status).send(response.data))
-    .catch(error => res.status(400).send({'message': requestError}))
+    .catch(error => res.status(error.response.status).send(error.response.data))
 });
 
 router.post(`/api/${process.env.API_VERSION}/emails`, requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.post(emailBaseUrl + req.path, req.body, req.headers)
     .then(response => res.status(response.status).send(response.data))
-    .catch(error => res.status(400).send({'message': requestError}))
+    .catch(error => res.status(error.response.status).send(error.response.data))
 });
 
 router.patch(`/api/${process.env.API_VERSION}/emails/:id`, requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.patch(emailBaseUrl + req.path, req.body, req.headers)
     .then(response => res.status(response.status).send(response.data))
-    .catch(error => res.status(400).send({'message': requestError}))
+    .catch(error => res.status(error.response.status).send(error.response.data))
 });
 
 router.put(`/api/${process.env.API_VERSION}/emails/:id`, requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.put(emailBaseUrl + req.path, req.body, req.headers)
     .then(response => res.status(response.status).send(response.data))
-    .catch(error => res.status(400).send({'message': requestError}))
+    .catch(error => res.status(error.response.status).send(error.response.data))
 });
 
 router.delete(`/api/${process.env.API_VERSION}/emails/:id`, requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.delete(emailBaseUrl + req.path, req.body, req.headers)
     .then(response => res.status(response.status).send(response.data))
-    .catch(error => res.status(400).send({'message': requestError}))
+    .catch(error => res.status(error.response.status).send(error.response.data))
 });
 
 export {router as emailRouter};
--- src/server/routes/subscriptionRouter.ts
+++ src/server/routes/subscriptionRouter.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response} from 'express';
 import axios from 'axios';
-import {emailBaseUrl, requestError, subscriptionBaseUrl} from '../utils/constants/stringUtils';
+import {requestError, subscriptionBaseUrl} from '../utils/constants/stringUtils';
 import {requireAuth} from '@adidastest-phillip/common';
 
 const router = express.Router();
@@ -9,70 +9,70 @@ router.get([`/api/${process.env.API_VERSION}/subscriptions/:id`, `/api/${process
   // @ts-ignore
   axios.get(subscriptionBaseUrl + req.path, req.headers)
     .then(response => res.status(response.status).send(response.data))
-    .catch(error => res.status(400).send({'message': requestError}))
+    .catch(error => res.status(error.response.status).send(error.response.data))
 });
 
 router.post(`/api/${process.env.API_VERSION}/subscriptions`, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.post(subscriptionBaseUrl + req.path, req.body, req.headers)
     .then(response => res.status(response.status).send(response.data))
-    .catch(error => res.status(400).send({'message': requestError}))
+    .catch(error => res.status(error.response.status).send(error.response.data))
 });
 
 router.put(`/api/${process.env.API_VERSION}/subscriptions/:id`, requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.put(subscriptionBaseUrl + req.path, req.body, req.headers)
     .then(response => res.status(response.status).send(response.data))
-    .catch(error => res.status(400).send({'message': requestError}))
+    .catch(error => res.status(error.response.status).send(error.response))
 });
 
 router.patch(`/api/${process.env.API_VERSION}/subscriptions/:id`, requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.patch(subscriptionBaseUrl + req.path, req.body, req.headers)
     .then(response => res.status(response.status).send(response.data))
-    .catch(error => res.status(400).send({'message': requestError}))
+    .catch(error => res.status(error.response.status).send(error.response.data))
 });
 
 router.delete(`/api/${process.env.API_VERSION}/subscriptions/:id`, requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.delete(subscriptionBaseUrl + req.path, req.body, req.headers)
     .then(response => res.status(response.status).send(response.data))
-    .catch(error => res.status(400).send({'message': requestError}))
+    .catch(error => res.status(error.response.status).send(error.response.data))
 });
 
 router.get([`/api/${process.env.API_VERSION}/campaigns/:id`, `/api/${process.env.API_VERSION}/campaigns`], requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.get(subscriptionBaseUrl + req.path, req.headers)
     .then(response => res.status(response.status).send(response.data))
-    .catch(error => res.status(400).send({'message': requestError}))
+    .catch(error => res.status(error.response.status).send(error.response.data))
 });
 
 router.post(`/api/${process.env.API_VERSION}/campaigns`, requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.post(subscriptionBaseUrl + req.path, req.body, req.headers)
     .then(response => res.status(response.status).send(response.data))
-    .catch(error => res.status(400).send({'message': requestError}))
+    .catch(error => res.status(error.response.status).send(error.response.data))
 });
 
 router.put(`/api/${process.env.API_VERSION}/campaigns/:id`, requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.put(subscriptionBaseUrl + req.path, req.body, req.headers)
     .then(response => res.status(response.status).send(response.data))
-    .catch(error => res.status(400).send({'message': requestError}))
+    .catch(error => res.status(error.response.status).send(error.response.data))
 });
 
 router.patch(`/api/${process.env.API_VERSION}/campaigns/:id`, requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.patch(subscriptionBaseUrl + req.path, req.body, req.headers)
     .then(response => res.status(response.status).send(response.data))
-    .catch(error => res.status(400).send({'message': requestError}))
+    .catch(error => res.status(error.response.status).send(error.response.data))
 });
 
 router.delete(`/api/${process.env.API_VERSION}/campaigns/:id`, requireAuth, async (req: Request, res: Response) => {
   // @ts-ignore
   axios.delete(subscriptionBaseUrl + req.path, req.body, req.headers)
     .then(response => res.status(response.status).send(response.data))
-    .catch(error => res.status(400).send({'message': requestError}))
+    .catch(error => res.status(error.response.status).send(error.response.data))
 });
 
 export {router as subscriptionRouter};
